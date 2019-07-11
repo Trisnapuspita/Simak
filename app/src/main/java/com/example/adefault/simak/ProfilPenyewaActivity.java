7fe68ed116a1bd30426a7c98771875694ab95e59
Trisnapuspita <trisna0401@gmail.com>
2019-07-11 20:51:30 +0700

    yang baru

---
+++ app/src/main/java/com/example/adefault/simak/ProfilPenyewaActivity.java
@@ -0,0 +1,13 @@
+package com.example.adefault.simak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilPenyewaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_penyewa);
+    }
+}
